a3941a3cfe7dc9be66021efb957efb3c390ed240
unknown <shireenkk28@gmail.com>
2021-07-16 18:41:26 +0530

    [shireen]Added: Addition of two numbers

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,8 +6,34 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Calculator");      
+            Console.WriteLine("Enter the action to be performed");
+            Console.WriteLine("Press 1 for Addition");
+            int action = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 1st input");
+            int input_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd input");
+            int input_2 = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            switch (action)
+            {
+                case 1:
+                    {
+                        result = Addition(input_1, input_2);
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Wrong action!! try again");
+                    break;
+            }
+            Console.WriteLine("The result is {0}", result);
+            Console.ReadKey();
         }
+        //Addition  
+        public static int Addition(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+    }
        
     }
-}
