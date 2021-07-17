87066d0f1f290a9e16d9a8117e8ba6f6b9a70c91
[Prashik] <[prashikjaware123@gmail.com]>
2021-07-17 09:29:17 +0530

    [Prashik]Added: While loop for continuation if wrong choice is given

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,61 +8,73 @@ namespace Calculator
         {
             Console.WriteLine("Welcome to Calculator");
 
-            Console.WriteLine("Enter the action to be performed");
-            Console.WriteLine("Press 1 for Addition");
-
-            Console.WriteLine("Press 2 for Subtraction");
-
-            Console.WriteLine("Press 3 for Division");
-
-            Console.WriteLine("Press 4 for Multiplication");
-
-
-            Console.WriteLine("Press 5 for Modulation");
-
-            int action = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 1st input");
-            int input_1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 2nd input");
-            int input_2 = Convert.ToInt32(Console.ReadLine());
             decimal result = 0;
-            switch (action)
+
+            int action = 0;
+
+            while (0 >= action || action >= 6)
             {
-                case 1:
+                Console.WriteLine("Enter the action to be performed");
+                Console.WriteLine("Press 1 for Addition");
 
-                    {
-                        result = Addition(input_1, input_2);
-                        break;
-                    }
-                case 2:
+                Console.WriteLine("Press 2 for Subtraction");
 
-                    {
-                        result = MoreMethods.Subtraction(input_1, input_2);
-                        break;
-                    }
-                case 3:
+                Console.WriteLine("Press 3 for Division");
 
-                    {
-                        result = Division(input_1, input_2);
+                Console.WriteLine("Press 4 for Multiplication");
 
-                        break;
-                    }
+                Console.WriteLine("Press 5 for Modulation");
 
-                case 4:
-                    {
-                        result = Multiplication(input_1, input_2);
+                Console.WriteLine("Enter your choice from 1 to 5:");
+                action = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter 1st input");
+                int input_1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter 2nd input");
+                int input_2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (action)
+                {
+                    case 1:
+
+                        {
+                            result = Addition(input_1, input_2);
+                            break;
+                        }
+                    case 2:
+
+                        {
+                            result = MoreMethods.Subtraction(input_1, input_2);
+                            break;
+                        }
+                    case 3:
+
+                        {
+                            result = Division(input_1, input_2);
+
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            result = Multiplication(input_1, input_2);
+                            break;
+                        }
+                    case 5:
+                        {
+                            result = Modulation(input_1, input_2);
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("Wrong choice!! try again");
                         break;
-                    }
-                case 5:
-                    {
-                        result = Modulation(input_1, input_2);
-                        break;
-                    }
-                default:
-                    Console.WriteLine("Wrong action!! try again");
-                    break;
+                }
+
+                if (0 < action && action < 6)
+                {
+                    Console.WriteLine("The result is {0}", result);
+                }
             }
-            Console.WriteLine("The result is {0}", result);
+
             Console.ReadKey();
 
         }
@@ -99,6 +111,11 @@ namespace Calculator
         {
             int result = Math.Abs(input_1 - input_2);
             return result;
+        }
+
+        public static void Inputs()
+        {
+
         }
 
     }
